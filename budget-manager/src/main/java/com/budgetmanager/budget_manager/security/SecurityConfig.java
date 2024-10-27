82fd349844c9211bca72b376d6b548f3df03101e
Ghalia789 <ghaliabellalouna@gmail.com>
2024-10-27 16:41:12 +0100

    init security disabled for now

---
+++ budget-manager/src/main/java/com/budgetmanager/budget_manager/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.budgetmanager.budget_manager.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/**"); // Disable security for all endpoints
+    }
+}
